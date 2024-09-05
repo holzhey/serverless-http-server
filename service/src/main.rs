7a878f84ff993ac41b5fe1c3f3237badd91cf126
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2024-09-05 16:58:26 +0200

    Remove non used middleware

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,29 +1,12 @@
 use std::panic::set_hook;
 
-use axum::middleware::from_fn;
 use axum::Router;
-use lambda_http::request::RequestContext::ApiGatewayV1;
 use lambda_http::{run, tracing, Error};
 use tower_http::services::ServeFile;
 use tracing_panic::panic_hook;
 
 pub mod routes;
 pub mod view;
-
-async fn mw_sample(
-    req: axum::extract::Request,
-    next: axum::middleware::Next,
-) -> impl axum::response::IntoResponse {
-    let context = req
-        .extensions()
-        .get::<lambda_http::request::RequestContext>();
-    tracing::info!("Context {:?}", context);
-    if let Some(ApiGatewayV1(ctx)) = context {
-        tracing::info!("RequestId = {:?}", ctx.request_id);
-        tracing::info!("Stage= {:?}", ctx.stage);
-    }
-    next.run(req).await
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -45,7 +28,7 @@ fn app() -> Router {
     for route in routes::get_all() {
         app = app.merge(route);
     }
-    app.route_layer(from_fn(mw_sample))
+    app
 }
 
 #[cfg(test)]
